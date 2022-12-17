32be3d39b99148af36bbf833f94a2aca97215be9
dzarkV <dfvargasp@unal.edu.co>
2022-12-16 20:23:19 -0500

    New red color

--- src/test/java/com/challenge/stepdefinitions/AddRegistryStepDefinition.java
+++ src/test/java/com/challenge/stepdefinitions/AddRegistryStepDefinition.java
@@ -28,6 +28,7 @@ public class AddRegistryStepDefinition {
 
     private static final Color RED = Color.fromString("rgb(220, 53, 69)") ;
     private static final Color DARK_RED = Color.fromString("rgb(212, 148, 158)");
+    private static final Color TOO_DARK_RED = Color.fromString("rgb(217, 90, 104)");
 
     private final List<UserData> userDataList = fromCsvToUsersData();
     private final List<String> webElementsRegistry = fromJsonFileToListString("Flow 1");
@@ -64,7 +65,9 @@ public class AddRegistryStepDefinition {
         theActorInTheSpotlight().should(
                 seeThat("Field incompleted is required",
                         ValidateField.colorFieldWhenEmailIsEmpty(),
-                        either(equalTo(RED.asRgb())).or(equalTo(DARK_RED.asRgb()))
+                        either(equalTo(RED.asRgb()))
+                                .or(equalTo(DARK_RED.asRgb()))
+                                .or(equalTo(TOO_DARK_RED.asRgb()))
                 )  // color rojo de la alerta en rgb
         );
     }

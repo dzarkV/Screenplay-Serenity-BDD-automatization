1dd19bdb31f700e1e833d8694b4ba2d9df5785c0
dzarkV <dfvargasp@unal.edu.co>
2022-12-22 16:46:44 -0500

    Refactor and delete comments

--- src/main/java/com/challenge/exceptions/ExcepcionGeneral.java
+++ src/main/java/com/challenge/exceptions/ExcepcionGeneral.java
@@ -2,8 +2,6 @@ package com.challenge.exceptions;
 
 public class ExcepcionGeneral extends AssertionError {
 
-//    private static final long serialVersionUID = 1L;
-
     public ExcepcionGeneral(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/com/challenge/utilities/ErrorMessage.java
+++ src/main/java/com/challenge/utilities/ErrorMessage.java
@@ -2,6 +2,8 @@ package com.challenge.utilities;
 
 public class ErrorMessage {
 
+    private ErrorMessage(){}
+
     public static final String MSG_ERROR_COMPARE = "El valor obtenido difiere del esperado.";
 
 }
